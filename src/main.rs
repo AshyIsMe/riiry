0812908f720cbef3f9674453adeff38096c4c6f7
Aaron Ash <aaron.ash@gmail.com>
2019-03-16 17:06:34 +1100

    gtk hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+extern crate gtk;
+
+use gtk::prelude::*;
+
+
+use gtk::{Button, Window, WindowType};
+
 fn main() {
     println!("Hello, world!");
+
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
+    }
+
+    let window = Window::new(WindowType::Toplevel);
+    window.set_title("riiry launcher");
+    window.set_default_size(350, 70);
+    let button = Button::new_with_label("Fuzzy Click me!");
+    window.add(&button);
+    window.show_all();
+
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
+
+    button.connect_clicked(|_| {
+        println!("Clicked!");
+    });
+
+    gtk::main();
 }
