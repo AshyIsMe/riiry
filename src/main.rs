bb60c7844b449e845796c7fc896e0a993611f487
Aaron Ash <aaron.ash@gmail.com>
2019-03-16 19:31:32 +1100

    Display fd output for current directory

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ extern crate gtk;
 use gtk::prelude::*;
 
 
-use gtk::{Button, Window, WindowType};
+use gtk::{Button, TextView, Window, WindowType};
+use std::process::{Command, Stdio};
+use std::io::{self, Write};
 
 fn main() {
     println!("Hello, world!");
@@ -13,11 +15,24 @@ fn main() {
         return;
     }
 
-    let window = Window::new(WindowType::Toplevel);
+    //let window = Window::new(WindowType::Toplevel);
+    let window = Window::new(WindowType::Popup);
     window.set_title("riiry launcher");
     window.set_default_size(350, 70);
     let button = Button::new_with_label("Fuzzy Click me!");
-    window.add(&button);
+
+    let textview = TextView::new();
+    //textview.set_cursor_visible(false);
+    let scrolledtextview = gtk::ScrolledWindow::new(gtk::NONE_ADJUSTMENT, gtk::NONE_ADJUSTMENT);
+    scrolledtextview.set_policy(gtk::PolicyType::Automatic, gtk::PolicyType::Automatic);
+    scrolledtextview.add(&textview);
+
+    // Pack widgets vertically.
+    let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
+    vbox.pack_start(&button, false, false, 0);
+    vbox.pack_start(&scrolledtextview, true, true, 0);
+    window.add(&vbox);
+
     window.show_all();
 
     window.connect_delete_event(|_, _| {
@@ -28,6 +43,19 @@ fn main() {
     button.connect_clicked(|_| {
         println!("Clicked!");
     });
+
+    let cmd = Command::new("fd")
+        .arg("-pa")
+        .arg(".")
+        .output()
+        .expect("Failed to run fd");
+
+    //io::stdout().write_all(&cmd.stdout).unwrap();
+    //io::stderr().write_all(&cmd.stderr).unwrap();
+
+    let strlines = String::from_utf8(cmd.stdout).unwrap();
+    let buffer = textview.get_buffer().unwrap();
+    buffer.insert_at_cursor(&strlines);
 
     gtk::main();
 }
