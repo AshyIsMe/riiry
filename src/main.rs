e2d34fbfedfec5854c330e1d2f07a86c5d814ed4
Aaron Ash <aaron.ash@gmail.com>
2019-03-20 23:36:40 +1100

    Wire up user input events

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,29 @@ use gtk::prelude::*;
 
 
 use gtk::{Button, Entry, TextView, Window, WindowType};
-use std::process::{Command, Stdio};
-use std::io::{self, Write};
+use std::process::{Command};
+//use std::io::{self, Write};
+
+fn get_files() -> String {
+    let cmd = Command::new("fd")
+        .arg("-pa")
+        .arg(".")
+        .output()
+        .expect("Failed to run fd");
+
+    //io::stdout().write_all(&cmd.stdout).unwrap();
+    //io::stderr().write_all(&cmd.stderr).unwrap();
+
+    let files = String::from_utf8(cmd.stdout).unwrap();
+    return files;
+}
+
+fn filter_lines(query: &str, strlines: &str) -> String {
+    let results = String::from("foo\nbar\nbaz");
+    // AA TODO - actually filter here
+    println!("We should really actually query for: {}, in {}", query, strlines);
+    return results;
+}
 
 fn main() {
     println!("Hello, world!");
@@ -15,6 +36,8 @@ fn main() {
         return;
     }
 
+    let full_files_list = get_files();
+
     //let window = Window::new(WindowType::Toplevel);
     let window = Window::new(WindowType::Popup);
     window.set_title("riiry launcher");
@@ -23,17 +46,21 @@ fn main() {
 
     let entry = Entry::new();
 
-    let textview = TextView::new();
-    //textview.set_cursor_visible(false);
-    let scrolledtextview = gtk::ScrolledWindow::new(gtk::NONE_ADJUSTMENT, gtk::NONE_ADJUSTMENT);
-    scrolledtextview.set_policy(gtk::PolicyType::Automatic, gtk::PolicyType::Automatic);
-    scrolledtextview.add(&textview);
+    let text_view = TextView::new();
+    text_view.set_cursor_visible(false);
+    text_view.set_editable(false);
+    let scrolled_text_view = gtk::ScrolledWindow::new(gtk::NONE_ADJUSTMENT, gtk::NONE_ADJUSTMENT);
+    scrolled_text_view.set_policy(gtk::PolicyType::Automatic, gtk::PolicyType::Automatic);
+    scrolled_text_view.add(&text_view);
+
+    let buffer = text_view.get_buffer().unwrap();
+    buffer.insert_at_cursor(&full_files_list);
 
     // Pack widgets vertically.
     let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
     vbox.pack_start(&entry, false, false, 0);
     vbox.pack_start(&button, false, false, 0);
-    vbox.pack_start(&scrolledtextview, true, true, 0);
+    vbox.pack_start(&scrolled_text_view, true, true, 0);
     window.add(&vbox);
 
     window.show_all();
@@ -47,18 +74,13 @@ fn main() {
         println!("TODO: Actually launch with xdg-open lolo");
     });
 
-    let cmd = Command::new("fd")
-        .arg("-pa")
-        .arg(".")
-        .output()
-        .expect("Failed to run fd");
-
-    //io::stdout().write_all(&cmd.stdout).unwrap();
-    //io::stderr().write_all(&cmd.stderr).unwrap();
-
-    let strlines = String::from_utf8(cmd.stdout).unwrap();
-    let buffer = textview.get_buffer().unwrap();
-    buffer.insert_at_cursor(&strlines);
+    //entry.connect_activate(move |e| {
+    entry.connect_changed(move |e| {
+        let buffer = e.get_buffer();
+        let query = buffer.get_text();
+        let results = filter_lines(&query, &full_files_list.clone());
+        println!("{}", results);
+    });
 
     gtk::main();
 }
