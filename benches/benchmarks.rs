6dcd87ad8ebc83c725b2c0fd99b2b8a3531e3a19
Aaron Ash <aaron.ash@gmail.com>
2019-04-07 12:11:23 +1000

    Fix benchmark names

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -9,19 +9,19 @@ use riiry::files;
 use riiry::filter;
 
 fn bench_get_apps(c: &mut Criterion) {
-    c.bench_function("filter_lines()", |b| {
+    c.bench_function("bench_get_apps()", |b| {
         b.iter(|| apps::get_apps())
     });
 }
 
 fn bench_get_files(c: &mut Criterion) {
-    c.bench_function("filter_lines()", |b| {
+    c.bench_function("bench_get_files()", |b| {
         b.iter(|| files::get_home_files())
     });
 }
 
 fn bench_filter_lines_apps(c: &mut Criterion) {
-    c.bench_function("filter_lines()", |b| {
+    c.bench_function("bench_filter_lines_apps()", |b| {
         b.iter(|| {
             let apps = apps::get_apps().unwrap();
             filter::filter_lines("firefox", &apps)
@@ -30,7 +30,7 @@ fn bench_filter_lines_apps(c: &mut Criterion) {
 }
 
 fn bench_filter_lines_files(c: &mut Criterion) {
-    c.bench_function("filter_lines()", |b| {
+    c.bench_function("bench_filter_lines_files()", |b| {
         b.iter(|| {
             let files = files::get_home_files().unwrap();
             filter::filter_lines("firefox", &files)
@@ -44,5 +44,6 @@ criterion_group!(benches,
                  bench_get_apps,
                  bench_get_files,
                  bench_filter_lines_apps,
-                 bench_filter_lines_files);
+                 bench_filter_lines_files
+                 );
 criterion_main!(benches);
